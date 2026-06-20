7c19e4feaa7889c3e47d38ce0ed8af4b675773e2
eliadematoslima <eliadematoslima@gmail.com>
2026-06-20 21:25:35 +0000

    feat: insere grupos oficiais J, K e L completando as 48 seleções reais da copa 2026

--- js/data/teams.js
+++ js/data/teams.js
@@ -3,74 +3,74 @@
 export const groupsData = {
     A: [
         { id: "MEX", name: "México", flag: "https://flagcdn.com/mx.svg" },
-        { id: "USA", name: "Estados Unidos", flag: "https://flagcdn.com/us.svg" },
-        { id: "CAN", name: "Canadá", flag: "https://flagcdn.com/ca.svg" },
-        { id: "NZL", name: "Nova Zelândia", flag: "https://flagcdn.com/nz.svg" }
+        { id: "KOR", name: "Coreia do Sul", flag: "https://flagcdn.com/kr.svg" },
+        { id: "CZE", name: "Chéquia", flag: "https://flagcdn.com/cz.svg" },
+        { id: "RSA", name: "África do Sul", flag: "https://flagcdn.com/za.svg" }
     ],
     B: [
-        { id: "BRA", name: "Brasil", flag: "https://flagcdn.com/br.svg" },
-        { id: "CRO", name: "Croácia", flag: "https://flagcdn.com/hr.svg" },
-        { id: "NGA", name: "Nigéria", flag: "https://flagcdn.com/ng.svg" },
-        { id: "KOR", name: "Coreia do Sul", flag: "https://flagcdn.com/kr.svg" }
+        { id: "CAN", name: "Canadá", flag: "https://flagcdn.com/ca.svg" },
+        { id: "SUI", name: "Suíça", flag: "https://flagcdn.com/ch.svg" },
+        { id: "BIH", name: "Bósnia e H.", flag: "https://flagcdn.com/ba.svg" },
+        { id: "QAT", name: "Catar", flag: "https://flagcdn.com/qa.svg" }
     ],
     C: [
-        { id: "ARG", name: "Argentina", flag: "https://flagcdn.com/ar.svg" },
+        { id: "BRA", name: "Brasil", flag: "https://flagcdn.com/br.svg" },
         { id: "MAR", name: "Marrocos", flag: "https://flagcdn.com/ma.svg" },
-        { id: "POL", name: "Polônia", flag: "https://flagcdn.com/pl.svg" },
-        { id: "KSA", name: "Arábia Saudita", flag: "https://flagcdn.com/sa.svg" }
+        { id: "SCO", name: "Escócia", flag: "https://flagcdn.com/gb-sct.svg" },
+        { id: "HAI", name: "Haiti", flag: "https://flagcdn.com/ht.svg" }
     ],
     D: [
-        { id: "FRA", name: "França", flag: "https://flagcdn.com/fr.svg" },
+        { id: "USA", name: "Estados Unidos", flag: "https://flagcdn.com/us.svg" },
         { id: "AUS", name: "Austrália", flag: "https://flagcdn.com/au.svg" },
-        { id: "EGY", name: "Egito", flag: "https://flagcdn.com/eg.svg" },
-        { id: "PER", name: "Peru", flag: "https://flagcdn.com/pe.svg" }
+        { id: "PAR", name: "Paraguai", flag: "https://flagcdn.com/py.svg" },
+        { id: "TUR", name: "Turquia", flag: "https://flagcdn.com/tr.svg" }
     ],
     E: [
-        { id: "ESP", name: "Espanha", flag: "https://flagcdn.com/es.svg" },
-        { id: "JPN", name: "Japão", flag: "https://flagcdn.com/jp.svg" },
-        { id: "CMR", name: "Camarões", flag: "https://flagcdn.com/cm.svg" },
-        { id: "CRC", name: "Costa Rica", flag: "https://flagcdn.com/cr.svg" }
+        { id: "CIV", name: "Costa do Marfim", flag: "https://flagcdn.com/ci.svg" },
+        { id: "GER", name: "Alemanha", flag: "https://flagcdn.com/de.svg" },
+        { id: "ECU", name: "Equador", flag: "https://flagcdn.com/ec.svg" },
+        { id: "CUW", name: "Curaçao", flag: "https://flagcdn.com/cw.svg" }
     ],
     F: [
-        { id: "GER", name: "Alemanha", flag: "https://flagcdn.com/de.svg" },
-        { id: "BEL", name: "Bélgica", flag: "https://flagcdn.com/be.svg" },
-        { id: "ARG_2", name: "Argélia", flag: "https://flagcdn.com/dz.svg" },
-        { id: "JAM", name: "Jamaica", flag: "https://flagcdn.com/jm.svg" }
+        { id: "NED", name: "Países Baixos", flag: "https://flagcdn.com/nl.svg" },
+        { id: "SWE", name: "Suécia", flag: "https://flagcdn.com/se.svg" },
+        { id: "JPN", name: "Japão", flag: "https://flagcdn.com/jp.svg" },
+        { id: "TUN", name: "Tunísia", flag: "https://flagcdn.com/tn.svg" }
     ],
     G: [
-        { id: "POR", name: "Portugal", flag: "https://flagcdn.com/pt.svg" },
-        { id: "URU", name: "Uruguai", flag: "https://flagcdn.com/uy.svg" },
-        { id: "GHA", name: "Gana", flag: "https://flagcdn.com/gh.svg" },
-        { id: "SCO", name: "Escócia", flag: "https://flagcdn.com/gb-sct.svg" }
+        { id: "NZL", name: "Nova Zelândia", flag: "https://flagcdn.com/nz.svg" },
+        { id: "IRN", name: "Irã", flag: "https://flagcdn.com/ir.svg" },
+        { id: "BEL", name: "Bélgica", flag: "https://flagcdn.com/be.svg" },
+        { id: "EGY", name: "Egito", flag: "https://flagcdn.com/eg.svg" }
     ],
     H: [
-        { id: "ITA", name: "Itália", flag: "https://flagcdn.com/it.svg" },
-        { id: "COL", name: "Colômbia", flag: "https://flagcdn.com/co.svg" },
-        { id: "SEN", name: "Senegal", flag: "https://flagcdn.com/sn.svg" },
-        { id: "OMA", name: "Omã", flag: "https://flagcdn.com/om.svg" }
+        { id: "URU", name: "Uruguai", flag: "https://flagcdn.com/uy.svg" },
+        { id: "KSA", name: "Arábia Saudita", flag: "https://flagcdn.com/sa.svg" },
+        { id: "ESP", name: "Espanha", flag: "https://flagcdn.com/es.svg" },
+        { id: "CPV", name: "Cabo Verde", flag: "https://flagcdn.com/cv.svg" }
     ],
     I: [
-        { id: "NED", name: "Países Baixos", flag: "https://flagcdn.com/nl.svg" },
-        { id: "ECU", name: "Equador", flag: "https://flagcdn.com/ec.svg" },
-        { id: "TUN", name: "Tunisia", flag: "https://flagcdn.com/tn.svg" },
+        { id: "NOR", name: "Noruega", flag: "https://flagcdn.com/no.svg" },
+        { id: "FRA", name: "França", flag: "https://flagcdn.com/fr.svg" },
+        { id: "SEN", name: "Senegal", flag: "https://flagcdn.com/sn.svg" },
         { id: "IRQ", name: "Iraque", flag: "https://flagcdn.com/iq.svg" }
     ],
     J: [
-        { id: "ENG", name: "Inglaterra", flag: "https://flagcdn.com/gb-eng.svg" },
-        { id: "CHI", name: "Chile", flag: "https://flagcdn.com/cl.svg" },
-        { id: "RSA", name: "África do Sul", flag: "https://flagcdn.com/za.svg" },
-        { id: "UZB", name: "Uzbequistão", flag: "https://flagcdn.com/uz.svg" }
+        { id: "ARG", name: "Argentina", flag: "https://flagcdn.com/ar.svg" },
+        { id: "AUT", name: "Áustria", flag: "https://flagcdn.com/at.svg" },
+        { id: "JOR", name: "Jordânia", flag: "https://flagcdn.com/jo.svg" },
+        { id: "ALG", name: "Argélia", flag: "https://flagcdn.com/dz.svg" }
     ],
     K: [
-        { id: "DEN", name: "Dinamarca", flag: "https://flagcdn.com/dk.svg" },
-        { id: "SUI", name: "Suíça", flag: "https://flagcdn.com/ch.svg" },
-        { id: "PAN", name: "Panamá", flag: "https://flagcdn.com/pa.svg" },
-        { id: "CHN", name: "China", flag: "https://flagcdn.com/cn.svg" }
+        { id: "COL", name: "Colômbia", flag: "https://flagcdn.com/co.svg" },
+        { id: "COD", name: "RD Congo", flag: "https://flagcdn.com/cd.svg" },
+        { id: "POR", name: "Portugal", flag: "https://flagcdn.com/pt.svg" },
+        { id: "UZB", name: "Uzbequistão", flag: "https://flagcdn.com/uz.svg" }
     ],
     L: [
-        { id: "AUT", name: "Áustria", flag: "https://flagcdn.com/at.svg" },
-        { id: "UKR", name: "Ucrânia", flag: "https://flagcdn.com/ua.svg" },
-        { id: "MLI", name: "Mali", flag: "https://flagcdn.com/ml.svg" },
-        { id: "QAT", name: "Catar", flag: "https://flagcdn.com/qa.svg" }
+        { id: "ENG", name: "Inglaterra", flag: "https://flagcdn.com/gb-eng.svg" },
+        { id: "GHA", name: "Gana", flag: "https://flagcdn.com/gh.svg" },
+        { id: "PAN", name: "Panamá", flag: "https://flagcdn.com/pa.svg" },
+        { id: "CRO", name: "Croácia", flag: "https://flagcdn.com/hr.svg" }
     ]
 };
